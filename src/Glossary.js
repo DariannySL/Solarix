b049162c12ca3419eba10228fc4daaf47428b034
Darianny <dariannysantana18@gmail.com>
2021-03-26 14:01:33 -0400

    Add design glossary

--- src/Glossary.js
+++ src/Glossary.js
@@ -1,9 +1,88 @@
 import React from 'react';
+import './GlossaryStyles.css';
 
 function Glossary() {
   return (
-    <div className="container">
-      <h1>Glosario de terminos</h1>
+    <div className="background">
+      <div className="container">
+        <div className="letters">
+          <ul>
+            <li className="glossary-link"><a href="#">A</a></li>
+            <li className="glossary-link"><a href="#">B</a></li>
+            <li className="glossary-link"><a href="glosario#C">C</a></li>
+            <li className="glossary-link"><a href="#">D</a></li>
+            <li className="glossary-link"><a href="#">E</a></li>
+            <li className="glossary-link"><a href="#">F</a></li>
+            <li className="glossary-link"><a href="#">G</a></li>
+            <li className="glossary-link"><a href="#">H</a></li>
+            <li className="glossary-link"><a href="#">I</a></li>
+            <li className="glossary-link"><a href="#">J</a></li>
+            <li className="glossary-link"><a href="#">K</a></li>
+            <li className="glossary-link"><a href="#">L</a></li>
+            <li className="glossary-link"><a href="#">M</a></li>
+            <li className="glossary-link"><a href="#">N</a></li>
+            <li className="glossary-link"><a href="#">O</a></li>
+            <li className="glossary-link"><a href="#">P</a></li>
+            <li className="glossary-link"><a href="#">Q</a></li>
+            <li className="glossary-link"><a href="#">R</a></li>
+            <li className="glossary-link"><a href="#">S</a></li>
+            <li className="glossary-link"><a href="#">T</a></li>
+            <li className="glossary-link"><a href="#">U</a></li>
+            <li className="glossary-link"><a href="#">V</a></li>
+            <li className="glossary-link"><a href="#">W</a></li>
+            <li className="glossary-link"><a href="#">X</a></li>
+            <li className="glossary-link"><a href="#">Y</a></li>
+            <li className="glossary-link"><a href="#">Z</a></li>
+          </ul>
+        </div>
+
+        <div className="definitions">
+          <h2>A</h2>
+          <h3>Asteroide</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec odio magna. 
+            Cras quis tellus condimentum, porta elit non, dictum elit. Maecenas condimentum 
+            a felis ac pharetra. Suspendisse vestibulum rutrum ipsum vel hendrerit. 
+            Donec tristique maximus est, ut condimentum nisl. Fusce eu posuere arcu. 
+            Ut ornare augue eu posuere tempor.</p>
+            <hr></hr>
+          <h3>Anillo</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec odio magna. 
+            Cras quis tellus condimentum, porta elit non, dictum elit. Maecenas condimentum 
+            a felis ac pharetra. Suspendisse vestibulum rutrum ipsum vel hendrerit. 
+            Donec tristique maximus est, ut condimentum nisl. Fusce eu posuere arcu. 
+            Ut ornare augue eu posuere tempor.</p>
+            <hr></hr>
+          <h3>Atmosfera</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec odio magna. 
+            Cras quis tellus condimentum, porta elit non, dictum elit. Maecenas condimentum 
+            a felis ac pharetra. Suspendisse vestibulum rutrum ipsum vel hendrerit. 
+            Donec tristique maximus est, ut condimentum nisl. Fusce eu posuere arcu. 
+            Ut ornare augue eu posuere tempor.</p>
+
+          <h2 id="#C" className="C">C</h2>
+          <h3>Corona</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec odio magna. 
+            Cras quis tellus condimentum, porta elit non, dictum elit. Maecenas condimentum 
+            a felis ac pharetra. Suspendisse vestibulum rutrum ipsum vel hendrerit. 
+            Donec tristique maximus est, ut condimentum nisl. Fusce eu posuere arcu. 
+            Ut ornare augue eu posuere tempor.</p>
+            <hr></hr>
+          <h3>Crácter</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec odio magna. 
+            Cras quis tellus condimentum, porta elit non, dictum elit. Maecenas condimentum 
+            a felis ac pharetra. Suspendisse vestibulum rutrum ipsum vel hendrerit. 
+            Donec tristique maximus est, ut condimentum nisl. Fusce eu posuere arcu. 
+            Ut ornare augue eu posuere tempor.</p>
+            <hr></hr>
+          <h3>Corteza</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec odio magna. 
+            Cras quis tellus condimentum, porta elit non, dictum elit. Maecenas condimentum 
+            a felis ac pharetra. Suspendisse vestibulum rutrum ipsum vel hendrerit. 
+            Donec tristique maximus est, ut condimentum nisl. Fusce eu posuere arcu. 
+            Ut ornare augue eu posuere tempor.</p>
+            
+        </div>
+      </div>
     </div>
   );
 }
